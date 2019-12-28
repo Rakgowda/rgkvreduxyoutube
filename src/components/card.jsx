8098fc2f234c0d9cb4f91f1458ad18f693e43d08
Rakgowda <rakshithgowdakv@gmail.com>
2019-12-29 01:03:20 +0530

    Add files via upload

---
+++ src/components/card.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class card extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            h
+        );
+    }
+}
+
+export default card;
