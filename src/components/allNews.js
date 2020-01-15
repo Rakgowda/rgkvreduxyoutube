b57f292ae6f1e1d93cc2d97b93e48a9b866a1c82
Rakgowda <rakshithgowdakv@gmail.com>
2020-01-15 14:30:26 +0530

    Add files via upload

--- src/components/allNews.js
+++ src/components/allNews.js
@@ -8,12 +8,27 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
-const useStyles = makeStyles({
-    card: {
-      maxWidth: 345,
-    },
-  });
+const useStyles = makeStyles(theme => ({
+  card: {
+    display: 'flex',
+    margin:"5px"
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+ 
+  cover: {
+    width: "300px",
+    height: "120px",
+
+    
+  },
+
+ 
+}));
  function AllNews(){
     const classes = useStyles();
     const newsstate = useSelector(state=>state.newsreducer)
@@ -25,26 +40,25 @@ const useStyles = makeStyles({
   
  return newsstate.newsloading?(<h1>loading</h1>):newsstate.newserr?(<h1>"error"</h1>):<div>{console.log(newsstate.newsdata)} {newsstate.newsdata["articles"].map(resultNews=>(
     
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="140"
-          image={resultNews.urlToImage}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {resultNews.title}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-           {resultNews.content}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+<Container maxWidth="sm">
+<Card className={classes.card}>
+      <CardMedia
+  className={classes.cover}
+  image={resultNews.urlToImage}
+  title={resultNews.title}
+/>
+  <div className={classes.details}>
+    <CardContent className={classes.content}>
+      <Typography component="p" variant="p">
+      {resultNews.title}
+      </Typography>
      
-    </Card>
+    </CardContent>
+  </div>
+ 
+</Card> 
+      </Container>
+ 
   
 ))}</div>
 
